a9b8ac6463df9f14ef6dd2b771441d6400e25bfb
-- Kanbhaa -- <anirudhdhsinhj@gmail.com>
2021-06-17 15:06:36 +0530

    Separated two fragments different adapters different models added viewpager removed bottombar added title change with fragment

---
+++ app/src/main/java/com/example/collapsingtoolbar/Activities/FullPhoto.java
@@ -0,0 +1,38 @@
+package com.example.collapsingtoolbar.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.example.collapsingtoolbar.Fragments.AllPhotosFragment;
+import com.example.collapsingtoolbar.Model.ImageModel;
+import com.example.collapsingtoolbar.R;
+import com.example.collapsingtoolbar.utils.GlideApp;
+
+import java.util.ArrayList;
+
+public class FullPhoto extends AppCompatActivity {
+
+    String TAG = "NewActivity";
+    ArrayList<ImageModel> arrayList;
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_photo);
+
+        String s = getIntent().getStringExtra("uri");
+        Log.d(TAG, "onCreate: Got Uri , "+s);
+
+        imageView = findViewById(R.id.fullPhoto);
+
+        GlideApp.with(getApplicationContext())
+                .load(Uri.parse(s))
+                .into(imageView);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/collapsingtoolbar/Activities/VideoPlay.java
@@ -0,0 +1,54 @@
+package com.example.collapsingtoolbar.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import com.example.collapsingtoolbar.R;
+import com.example.collapsingtoolbar.utils.GlideApp;
+
+public class VideoPlay extends AppCompatActivity {
+
+    VideoView videoView;
+    String TAG = "VideoActivity";
+    Boolean VIDEO_PLAYING = false;
+    MediaController mediaController;
+    ImageView thumbnail;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_play);
+
+
+        String s = getIntent().getStringExtra("uri");
+        Log.d(TAG, "onCreate: Got Uri , "+s);
+
+        thumbnail = findViewById(R.id.thumbnail);
+
+        GlideApp.with(getApplicationContext())
+                .load(Uri.parse(s))
+                .into(thumbnail);
+
+        videoView = findViewById(R.id.video);
+        videoView.setVideoURI(Uri.parse(s));
+
+        mediaController = new MediaController(this);
+
+
+    }
+
+    public  void videoPlay(View view)
+    {
+        thumbnail.setVisibility(View.GONE);
+        videoView.setVisibility(View.VISIBLE);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+        videoView.start();
+    }
+}
